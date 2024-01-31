7533bf4420b0b583657f9fb06188daa60495f15a
jcyalung <jyalung2022@gmail.com>
2024-01-31 00:40:09 -0800

    uploaded react

--- stedcs/src/App.js
+++ stedcs/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
+import './App.css';
+const curr_day = new Date();
 function App() {
+  const [studentID, setStudentID] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        Saint Edward Kids Club
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Saint Edward Kids Club Sign In<br />
+        {curr_day.toDateString()}
+        <input 
+            placeholder='Enter your Student ID'
+            value={studentID}
+            onChange={(e) => setStudentID(e.target.value)}
+        />
       </header>
     </div>
   );
