ece65449df5e9f2104fea18996a724539b2a3ca9
jcyalung <jyalung2022@gmail.com>
2024-02-10 00:25:25 -0800

    added comments to folders.py

--- api/folders.py
+++ api/folders.py
@@ -5,20 +5,23 @@ directories and files for the program to run.
 """
 import os
 
-CURRENT_DIR = "/users/jcyalung/stedcskidsclub/"
+# this variable should be the path name to the api folder.
+# ex. ~/stedcskidsclub/api 
+CURRENT_DIR = "/stedcskidsclub/api/"
 school_months = ["September", "October", "November", "December", "January", "February", "March", "April", "May", "June"]
 
+# this will create the Logs folder with all the appropriate months.
 def create_logs_folder():
     os.mkdir(CURRENT_DIR + "Logs")
     for month in school_months:
         os.mkdir(CURRENT_DIR + "Logs/" + month)
     
-    
+# this will create the signInSheets folder with the appropriate months.
 def create_sign_in_sheets_folder():
     os.mkdir(CURRENT_DIR + "signInSheets")
     for month in school_months:
         os.mkdir(CURRENT_DIR + "signInSheets/" + month)
-    
+
 if __name__ == "__main__":
     create_logs_folder()
     create_sign_in_sheets_folder()
